1d459c9fca3108f00713c696f73e65aac5a59da5
AtsiKi <a.atsuki0618@gmail.com>
2018-11-15 22:13:10 +0900

    GoForwardUnityChanプロジェクト　初めてのコミット
    教材を終了しました

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+
+    private float translatespeed = -0.03f;
+
+    private float deadline = -16f;
+
+    private float startpos = 15.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(translatespeed, 0, 0);
+
+        if(transform.position.x < deadline){
+            transform.position = new Vector2(startpos, 0);
+        }
+
+	}
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    private GameObject RunlengthText;
+
+    private GameObject GameOverText;
+
+    private bool isGameOver = false;
+
+    private float len = 0;
+
+    private float speed = 0.03f;
+
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+        this.RunlengthText = GameObject.Find("RunLength");
+
+        this.GameOverText = GameObject.Find("GameOver");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(isGameOver == false){
+
+            this.len += this.speed;
+
+            this.RunlengthText.GetComponent<Text>().text = "Distance:  " + len.ToString("F2") + "m";
+        }
+
+        if(isGameOver == true){
+            if(Input.GetMouseButton(0)){
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+		
+	}
+    public void GameOver(){
+        this.GameOverText.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
